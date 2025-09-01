2dfecc1c3f15d46649132a4bc6af872ba4b2d879
Daniela Muñoz <Daniela.munoza1@uqvirtual.edu.co>
2025-08-31 20:27:56 -0500

    Login implementado

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
 import { App } from './app/app';
+import { routes } from './app/app.routes'; // ← Importante: importar las rutas
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes), // ← Importante: configurar el router
+  ]
+}).catch(err => console.error(err));
